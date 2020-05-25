0becadac27f78109d98221b3dcbad0e5320c10e0
Cameron Skaggs <cameronskaggscs@gmail.com>
2020-05-25 13:43:42 -0700

    Added parsing for wires, circles, holes, and vias. Cleaned up code, added type conversion for convenience.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PyCBmodela
-A Python-based interface for the Modela MDX-15 3D Milling Machine.
+A Python-based interface for the Modela MDX-15 3D Milling Machine. At present, the board parse script is designed for
+use with gerber file created using Autodesk Eagle. Code written is inspired by nodela, a Node-based milling interface.
--- brd_parse.py
+++ brd_parse.py
@@ -1,12 +1,14 @@
 """
-    WIP .brd parse script for use with Modela MDX-15 milling machine. Current 
-    functionality is:
-        -Printing a list of layers and parts in the board file.
+    Current functionality is:
+        -Creating board object with wires, circles, holes, vias, parts, layers. 
 
     To run, place this script and a single .brd file in a directory and run
     >python3 brd_parse.py
 
-    22 May 2020
+    Ongoing Questions
+        -Consider polygons (e.g. ground) as wire?
+
+    25 May 2020
 """
 import sys
 
@@ -19,9 +21,23 @@ class Board:
         self.info = {
             'width' : -1,
             'height' : -1,
-            'wires' : '',
-            'circles' : '',
+            
+            # List of dictionaries with keys x1, y1, x2, y2, width, layer
+            'wires' : [],
+
+            # List of dictionaries with keys x, y, radius, width, layer
+            'circles' : [],
+
+            # List of dictionaries with keys x, y, drill
+            'holes' : [],
+
+            # List of dictionaries with keys x, y, extent, drill
+            'vias' : [],
+            
+            # List of dictionaries with keys name, value, library, package, rot, x, y
             'parts' : [],
+
+            # Dictionary with int keys and str values
             'layers' : {},
         }
 
@@ -44,15 +60,11 @@ def findbrd():
     except:
         print('Error opening .brd file.')
         raise
-        sys.exit(-1)
 
 
 
 """
-    Parse an XML file for layers and parts. 
-
-    ?? For layers actually used on physical board, 
-    parse for layers under <elements> or <signals> ??
+    Parse an XML file for board physical features.
 """
 def parseXML(XMLfile):
     import xml.etree.ElementTree as ET
@@ -60,31 +72,100 @@ def parseXML(XMLfile):
         tree = ET.parse(XMLfile)
         root = tree.getroot()
 
-        layerDict = {}
-        layersTag = next(root.iter('layers'))           # Access the layers tag
-        for layer in layersTag.iter('layer'):
-            layerNumber = int(layer.get('number'))
-            layerName = layer.get('name')
-            layerDict[layerNumber] = layerName
+        def parseWires():
+            wiresList = []
+            signalsTag = next(root.iter('signals'))                 # Access signals tag
+            for wireElement in signalsTag.iter('wire'):
+                """
+                    Will eventually need to check for valid coordinates
+                """
+                wiresList.append({
+                    'x1' : float(wireElement.get('x1')),
+                    'y1' : float(wireElement.get('y1')),
+                    'x2' : float(wireElement.get('x2')),
+                    'y2' : float(wireElement.get('y2')),
+                    'width' : float(wireElement.get('width')),
+                    'layer' : int(wireElement.get('layer'))
+                })
+            return wiresList
         
-        partsList = []
-        elementsTag = next(root.iter('elements'))       # Access the elements tag
-        for boardElement in elementsTag.iter('element'):
-            elementName = boardElement.get('name')
-            elementLibrary = boardElement.get('library')
-            partsList.append((elementName, elementLibrary))
+        def parseCircles():
+            circlesList = []
+            plainTag = next(root.iter('plain'))                     # Access the plain tag
+            for circle in plainTag.iter('circle'):
+                circlesList.append({
+                    'x' : float(circle.get('x')),
+                    'y' : float(circle.get('y')),
+                    'radius' : float(circle.get('radius')),
+                    'width' : float(circle.get('width')),
+                    'layer' : int(circle.get('layer'))
+                })                                                  # Note that circles are nothing more than holes at 
+            return circlesList                                      # a point. Add routes around the perimeter later
+
+        def parseHoles():
+            holesList = []
+            plainTag = next(root.iter('plain'))                     # Access the plain tag
+            for holeElement in plainTag.iter('hole'):
+                """
+                    Will eventually need to check for valid coordinates
+                """
+                holesList.append({
+                    'x' : float(holeElement.get('x')),
+                    'y' : float(holeElement.get('y')),
+                    'drill' : float(holeElement.get('drill'))
+                })
+            return holesList
+
+        def parseParts():
+            partsList = []
+            elementsTag = next(root.iter('elements'))               # Access the elements tag
+            for boardElement in elementsTag.iter('element'):
+                partsList.append({
+                    'name' : boardElement.get('name'),
+                    'library' : boardElement.get('library'),
+                    'package' : boardElement.get('package'),
+                    'value' : boardElement.get('value'),
+                    'x' : float(boardElement.get('x')),
+                    'y' : float(boardElement.get('y')),
+                    'rot' : boardElement.get('rot')                 # None if does not exist
+                })
+            return partsList
+            
+        def parseVias():
+            viasList = []
+            signalsTag = next(root.iter('signals'))                 # Access the signals tag
+            for viaElement in signalsTag.iter('via'):
+                viasList.append({
+                    'x' : float(viaElement.get('x')),
+                    'y' : float(viaElement.get('y')),
+                    'extent' : viaElement.get('extent'),
+                    'drill' : float(viaElement.get('drill')),
+                })
+            return viasList
+
+        def parseLayers():
+            layerDict = {}
+            layersTag = next(root.iter('layers'))                   # Access the layers tag
+            for layer in layersTag.iter('layer'):
+                layerNumber = int(layer.get('number'))
+                layerName = layer.get('name')
+                layerDict[layerNumber] = layerName
+            return layerDict
+
+
 
         myBoard = Board()
-        # myBoard.wires = wireList
-        # myBoard.circles = circleList
-        myBoard.parts = partsList
-        myBoard.layers = layerDict
+        myBoard.wires = parseWires()
+        myBoard.circles = parseCircles()
+        myBoard.holes = parseHoles()
+        myBoard.vias = parseVias()
+        myBoard.parts = parseParts()
+        myBoard.layers = parseLayers()
         return myBoard
 
     except:
         print('Error parsing .brd file.')
         raise
-        sys.exit(-1)
 
 
 
@@ -95,11 +176,49 @@ targetFile = findbrd()
 targetBoard = Board()
 targetBoard = parseXML(targetFile)
 
+
+
 # Proof of concept, will be deleted later
-print('Layers detected from layers XML tag:\n--------------------')
+print('Layers detected:\n--------------------')
 for layerNum in targetBoard.layers:
     print(str(layerNum) + ': ' + targetBoard.layers[layerNum])
 
-print('\nParts detected from elements XML tag:\n--------------------')
-for partPair in targetBoard.parts:
-    print(str(partPair[0]) + ': ' + partPair[1])
+
+
+print('\n\nParts detected:\n--------------------')
+for part in targetBoard.parts:
+    print(part['name'] + ' ' + part['value'] + ' ' + part['library'] + ' ' + part['package'] + ' detected at ' + str((part['x'], part['y'])) + ', rotation ' + str(part['rot']))
+
+
+
+print('\n\nWires detected:\n--------------------')
+wiresInEachLayer = [None] * 100
+for wire in targetBoard.wires:
+    if wiresInEachLayer[wire['layer']] is not None:
+        wiresInEachLayer[wire['layer']].append(((wire['x1'], wire['y1']), (wire['x2'], wire['y2'])))
+    else:
+        wiresInEachLayer[wire['layer']] = [((wire['x1'], wire['y1']), (wire['x2'], wire['y2']))]
+
+for layerNumber, wire in enumerate(wiresInEachLayer):
+    if wire is not None:
+        print('Layer ' + str(layerNumber) + ':')
+        for wireRoute in wire:
+            print(wireRoute)
+
+
+
+print('\n\nCircles detected:\n--------------------')
+for circle in targetBoard.circles:
+    print(str((circle['x'], circle['y'])) + ', radius: ' + str(circle['radius']) + ', width: ' + str(circle['width']) + ', layer: ' + str(circle['layer']))
+
+
+
+print('\n\nHoles detected:\n--------------------')
+for hole in targetBoard.holes:
+    print(str((hole['x'], hole['y'])) + ', drill size: ' + str(hole['drill']))
+
+
+
+print('\n\nVias detected:\n--------------------')
+for via in targetBoard.vias:
+    print(str((via['x'], hole['y'])) + ', drill size: ' + str(via['drill']) + ', extent: ' + via['extent'])
